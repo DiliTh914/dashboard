66b0532dc4945c9642f08d14727fee95c56b19ff
dilith <dilith914@gmail.com>
2023-07-17 20:02:42 +0530

    package restructure

--- frontend/src/components/navbar.component.js
+++ frontend/src/components/navbar.component.js
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">]
                 <Link to="/" className="navbar-brand">Exercise Tracker</Link>
                 <div className="collapse navbar-collapse">
-                    <ul className="navbar-nav mr-auto">
+                    <ul className="navbar-nav me-auto">
                         <li className="nav-item">
                             <Link to="/" className="nav-link">Exercises</Link>
                         </li>
